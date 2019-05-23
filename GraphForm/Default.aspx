4dd48cbf06174f905272c289a48159aff8522ad8
byksenia <44179551+byksenia@users.noreply.github.com>
2019-05-23 13:19:49 +0300

    Add files via upload

---
+++ GraphForm/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Вершина 1"></asp:Label>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Вершина 2"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Найти путь" />
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged" Width="480px"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click1" Text="Вес пути" />
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox6" runat="server" style="margin-left: 0px" Width="112px" OnTextChanged="TextBox6_TextChanged"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ GraphForm/Default.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+public partial class _Default : System.Web.UI.Page
+{
+    const int n = 11;
+    const int k = 10;
+    public class item
+    {
+        public int s, c, v;
+        public item(int s, int c, int v)
+        {
+            this.s = s;
+            this.c = c;
+            this.v = v;
+        }
+    };
+
+
+    item[] graph = new item[] { new item(1,2,3), new item(1,3,2), new item (2,4,5), new item(2,5,2), new item(3,6,1), new item(5,7,3)
+    ,new item (5,6,6), new item (7,10,8), new item (6,8,3), new item (8,9,4)};
+    int[] road = new int[n];
+    bool[] incl = new bool[n];
+    int[] way = new int[n];
+    int waylen;
+    int start, finish;
+    bool found;
+    int len;
+    int c_len;
+    bool fr = false;
+
+
+    int find(int s, int c)
+    {
+        for (int i = 0; i < k; i++)
+            if (graph[i].s == s && graph[i].c == c ||
+               graph[i].s == c && graph[i].c == s) return graph[i].v;
+        return 0;
+    }
+    void step(int s, int f, int p)
+    {
+        int c;
+        if (s == f)
+        {
+            found = true;
+            len = c_len;
+            waylen = p;
+            for (int i = 0; i < waylen; i++) way[i] = road[i];
+        }
+        else
+        {
+            for (c = 0; c < n; c++)
+            {
+                int w = find(s, c);
+                if ((w != 0) && !incl[c] && (len == 0 || c_len + w < len))
+                {
+                    road[p] = c;
+                    incl[c] = true;
+                    c_len += w;
+                    step(c, f, p + 1);
+                    road[p] = 0;
+                    incl[c] = false;
+                    c_len -= w;
+                }
+            }
+        }
+    }
+    void ways(int st, int fin)
+    {
+        //Инициализация данных:
+        for (int i = 0; i < n; i++)
+        {
+            road[i] = way[i] = 0; incl[i] = false;
+        }
+        len = c_len = waylen = 0;
+        start = st;
+        finish = fin;
+        road[0] = start;
+        incl[start] = true;
+        found = false;
+        step(start, finish, 1);
+    }
+
+    void check(bool tr)
+    {
+
+        if ((TextBox1.Text.Equals("")) || (TextBox2.Text.Equals("")))
+        {
+            //MessageBox:Show("Ошибка ввода! \nДанные не введены!", "ERROR", MessageBoxButtons.OK);
+            TextBox3.Text = "Вы не ввели все необходимые данные!";
+        }
+        else
+        {
+            string Str1 = TextBox1.Text.Trim();
+            string Str2 = TextBox2.Text.Trim();
+            int Num;
+            bool isNum = int.TryParse(Str1, out Num);
+            bool isNum2 = int.TryParse(Str2, out Num);
+            if (isNum && isNum2)
+            {
+                int a = Convert.ToInt32(Str1);
+                int b = Convert.ToInt32(Str2);
+                if ((a >= 1) || (a <= 10) && (b >= 1) || (b <= 10))
+                {
+                    ways(a, b);
+                    if (found)
+                    {
+                        for (int i = 0; i < waylen; i++)
+                        {
+                            if (tr == true)
+                            {
+                                TextBox3.Text = TextBox3.Text + " -> " + way[i];
+                            }
+                            else { }
+                        }
+                        if (tr == false)
+                        {
+                            TextBox6.Text = TextBox6.Text + len;
+                        }
+                    }
+                    else { TextBox3.Text = "Путь не найден!"; }
+                    // действие если строка - число
+                }
+                else { TextBox3.Text = "Путь не найден! "; }
+            }
+            else
+            {
+                TextBox3.Text = "Вы ввели некорректные данные!";
+                // действие если строка - не число
+
+            }
+        }
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TextBox3.Text = "";
+        check(true);
+        
+       
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        TextBox6.Text = "";
+        check(false);
+        
+    }
+
+    protected void TextBox6_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
+
+
+
+    
+        
